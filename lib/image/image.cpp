0761887053d1b4a2158fb11c087a3b43e4e77be8
Alban Fichet <alban.fichet@gmx.fr>
2020-10-14 01:32:43 +0200

    Reflect filters for convoluting on the edges

--- lib/image/image.cpp
+++ lib/image/image.cpp
@@ -24,7 +24,7 @@
 
 void image_convolve3x3(float *matrix, float *array_in, float *array_out, size_t width, size_t height)
 {
-#pragma omp parallel for
+  #pragma omp parallel for
   for (int y = 1; y < (int)height - 1; y++)
   {
     for (int x = 1; x < (int)width - 1; x++)
@@ -37,6 +37,93 @@ void image_convolve3x3(float *matrix, float *array_in, float *array_out, size_t 
       // clang-format on
     }
   }
+
+  // Doing the borders
+
+    #pragma omp parallel for
+    for (int x = 1; x < (int)width - 1; x++)
+    {
+        int y = 0;
+      // clang-format off
+      array_out[y * width + x] =
+        matrix[0] * array_in[(y + 1) * width + x - 1] + matrix[1] * array_in[(y + 1) * width + x] + matrix[2] * array_in[(y + 1) * width + x + 1] +
+        matrix[3] * array_in[(  y  ) * width + x - 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x + 1] +
+        matrix[6] * array_in[(y + 1) * width + x - 1] + matrix[7] * array_in[(y + 1) * width + x] + matrix[8] * array_in[(y + 1) * width + x + 1];
+      // clang-format on
+
+      y = height - 1;
+      // clang-format off
+      array_out[y * width + x] =
+              matrix[0] * array_in[(y - 1) * width + x - 1] + matrix[1] * array_in[(y - 1) * width + x] + matrix[2] * array_in[(y - 1) * width + x + 1] +
+              matrix[3] * array_in[(  y  ) * width + x - 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x + 1] +
+              matrix[6] * array_in[(y - 1) * width + x - 1] + matrix[7] * array_in[(y - 1) * width + x] + matrix[8] * array_in[(y - 1) * width + x + 1];
+      // clang-format on
+    }
+
+    #pragma omp parallel for
+    for (int y = 1; y < (int)height - 1; y++)
+    {
+      int x = 0;
+        // clang-format off
+        array_out[y * width + x] =
+          matrix[0] * array_in[(y - 1) * width + x + 1] + matrix[1] * array_in[(y - 1) * width + x] + matrix[2] * array_in[(y - 1) * width + x + 1] +
+          matrix[3] * array_in[(  y  ) * width + x + 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x + 1] +
+          matrix[6] * array_in[(y + 1) * width + x + 1] + matrix[7] * array_in[(y + 1) * width + x] + matrix[8] * array_in[(y + 1) * width + x + 1];
+        // clang-format on
+
+        x = width-1;
+
+        // clang-format off
+        array_out[y * width + x] =
+                matrix[0] * array_in[(y - 1) * width + x - 1] + matrix[1] * array_in[(y - 1) * width + x] + matrix[2] * array_in[(y - 1) * width + x - 1] +
+                matrix[3] * array_in[(  y  ) * width + x - 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x - 1] +
+                matrix[6] * array_in[(y + 1) * width + x - 1] + matrix[7] * array_in[(y + 1) * width + x] + matrix[8] * array_in[(y + 1) * width + x - 1];
+        // clang-format on
+    }
+
+    // And the edges
+
+    int x, y;
+
+    x = 0;
+    y = 0;
+
+    // clang-format off
+    array_out[y * width + x] =
+      matrix[0] * array_in[(y + 1) * width + x + 1] + matrix[1] * array_in[(y + 1) * width + x] + matrix[2] * array_in[(y + 1) * width + x + 1] +
+      matrix[3] * array_in[(  y  ) * width + x + 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x + 1] +
+      matrix[6] * array_in[(y + 1) * width + x + 1] + matrix[7] * array_in[(y + 1) * width + x] + matrix[8] * array_in[(y + 1) * width + x + 1];
+    // clang-format on
+
+    // x = 0
+    y = height - 1;
+
+    // clang-format off
+    array_out[y * width + x] =
+      matrix[0] * array_in[(y - 1) * width + x + 1] + matrix[1] * array_in[(y - 1) * width + x] + matrix[2] * array_in[(y - 1) * width + x + 1] +
+      matrix[3] * array_in[(  y  ) * width + x + 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x + 1] +
+      matrix[6] * array_in[(y - 1) * width + x + 1] + matrix[7] * array_in[(y - 1) * width + x] + matrix[8] * array_in[(y - 1) * width + x + 1];
+    // clang-format on
+
+    x = width - 1;
+    // y = height - 1;
+
+    // clang-format off
+    array_out[y * width + x] =
+      matrix[0] * array_in[(y - 1) * width + x - 1] + matrix[1] * array_in[(y - 1) * width + x] + matrix[2] * array_in[(y - 1) * width + x - 1] +
+      matrix[3] * array_in[(  y  ) * width + x - 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x - 1] +
+      matrix[6] * array_in[(y - 1) * width + x - 1] + matrix[7] * array_in[(y - 1) * width + x] + matrix[8] * array_in[(y - 1) * width + x - 1];
+    // clang-format on
+
+    // x = width - 1;
+    y = 0;
+
+    // clang-format off
+    array_out[y * width + x] =
+            matrix[0] * array_in[(y + 1) * width + x - 1] + matrix[1] * array_in[(y + 1) * width + x] + matrix[2] * array_in[(y + 1) * width + x - 1] +
+            matrix[3] * array_in[(  y  ) * width + x - 1] + matrix[4] * array_in[(  y  ) * width + x] + matrix[5] * array_in[(  y  ) * width + x - 1] +
+            matrix[6] * array_in[(y + 1) * width + x - 1] + matrix[7] * array_in[(y + 1) * width + x] + matrix[8] * array_in[(y + 1) * width + x - 1];
+    // clang-format on
 }
 
 
